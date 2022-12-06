d6a468e7d283b1bf45d7ee07534b7926f8d358ea
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2022-12-06 11:37:53 -0300

    Added more elements to the UI

--- SuperHero.UI/src/app/app.module.ts
+++ SuperHero.UI/src/app/app.module.ts
@@ -6,6 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EditHeroComponent } from './components/edit-hero/edit-hero.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {  MatButtonModule } from '@angular/material/button';
+import {MatCommonModule} from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -16,7 +22,13 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatCommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
